227133f3db7f6fbc093bc5288d4fed8d5cf787a5
Mariusz Zamolski <mariusz.zamolski@gmail.com>
2025-09-14 17:35:53 +0200

    feat/logback-encoder (#15)
    * fix: PROJECT_ID in JIB added 2
    
    * feat: logback-encoder added
    
    ---------
    
    Co-authored-by: Mariusz Zamolski <mariusz.zamolski@fingo.pl>

--- ktor-app/build.gradle.kts
+++ ktor-app/build.gradle.kts
@@ -5,8 +5,8 @@ dependencies {
     implementation(platform(libs.ktor.bom))
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-netty")
-    implementation("io.ktor:ktor-server-call-id")
     implementation("io.ktor:ktor-server-call-logging")
     implementation(libs.slf4j.api)
     implementation(libs.logback)
+    implementation(libs.logback.logstash.encoder)
 }
--- ktor-app/src/main/kotlin/com/zamolski/crkhello/app/Main.kt
+++ ktor-app/src/main/kotlin/com/zamolski/crkhello/app/Main.kt
@@ -3,12 +3,10 @@ package com.zamolski.crkhello.app
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.callid.*
 import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.slf4j.LoggerFactory
-import java.util.*
 
 fun main() {
     val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
@@ -36,20 +34,24 @@ fun main() {
 }
 
 fun Application.configureLogging(projectId: String) {
-    install(CallId) {
-        retrieve {
-            val header = it.request.headers["traceparent"]
-            header?.split("-")?.getOrNull(1)
-        }
-        generate {
-            UUID.randomUUID().toString()
-        }
-        verify { it.isNotBlank() }
-    }
+
     install(CallLogging) {
-        callIdMdc("trace_id")
+
         mdc("logging.googleapis.com/trace") { call ->
-            call.callId?.let { "projects/$projectId/traces/$it" }
+            val traceParent = call.request.headers["traceparent"]
+            val traceId = traceParent?.split("-")?.getOrNull(1)
+            traceId?.let { "projects/$projectId/traces/$it" }
+        }
+
+        mdc("logging.googleapis.com/spanId") { call ->
+            val traceParent = call.request.headers["traceparent"]
+            traceParent?.split("-")?.getOrNull(2)
+        }
+
+        mdc("logging.googleapis.com/trace_sampled") { call ->
+            val traceParent = call.request.headers["traceparent"]
+            val sampled = traceParent?.split("-")?.getOrNull(3)
+            if (sampled == "01") "true" else "false"
         }
     }
 
